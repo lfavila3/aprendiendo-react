84b5ff59a51da8019b88b1284eae0c5c67efb74e
lfavila3 <ferchoavila14@gmail.com>
2020-08-09 11:30:01 -0500

    Se ha realizado el emplo sencillo de React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./assets/images/logo.svg";
+import "./assets/css/App.css";
+
+//Importar componetes
+import MiComponente from "./components/MiComponente";
+
+function HolaMundo(nombre, edad) {
+  var presentacion = (
+    <div>
+      <h2>Hola soy {nombre}</h2>
+      <h3>Tengo {edad} anios</h3>
+    </div>
+  );
+
+  return presentacion;
+}
 
 function App() {
+  var nombre = "Fernando Avila";
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hola Bienvenido al curso de React de Fernando as</p>
+
+        {HolaMundo(nombre, 12)}
+
+        <section className="componentes">
+          <MiComponente />
+        </section>
       </header>
     </div>
   );
